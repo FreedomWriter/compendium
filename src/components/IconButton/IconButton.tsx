7aae09680a2460f84417f2cca4b10527efeb7e7b
Natalie Davis <natalienetworks@gmail.com>
2023-05-27 22:27:00 -0500

    Improve: refactored IconButton for consistency

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,77 +1,14 @@
 import React from "react";
-import styled, { css } from "styled-components";
 import { ButtonProps } from "types";
 import { useTheme } from "hooks";
 
-import { COLORS } from "utils";
-
-const Button = styled.button<ButtonProps>`
-  padding: 8px;
-  border: 2px solid
-    ${(props) => (props.isDarkMode ? COLORS.darkPrimary : COLORS.primary)};
-  border-radius: 50%;
-  background-color: ${(props) =>
-    props.isActive ? COLORS.secondary : "transparent"};
-  outline-color: transparent;
-  cursor: pointer;
-  color: ${(props) => (props.isActive ? COLORS.secondary : COLORS.primary)};
-  background-image: ${(props) =>
-    props.isActive ? 'url("/images/background.svg")' : undefined};
-
-  ${(props) =>
-    props.isActive &&
-    css`
-      background-image: url("/images/background.svg");
-    `}
-
-  &:focus {
-    outline-offset: 4px;
-    outline-color: ${COLORS.primary};
-  }
-
-  &:hover {
-    background-color: ${COLORS.secondary};
-    background-image: url("/images/background.svg");
-    border-radius: 50%;
-    border-color: ${COLORS.primary};
-    filter: brightness(80%);
-  }
-  ${(props) =>
-    props.isDarkMode &&
-    css`
-      background-image: url("/images/background.svg");
-      background-color: ${COLORS.darkPrimary};
-      border-color: ${COLORS.secondary};
-      background-image: url("/images/background.svg");
-      filter: brightness(65%);
-
-      &:hover {
-        background-color: ${COLORS.primary};
-        background-image: url("/images/background.svg");
-        border-radius: 50%;
-        border-color: ${COLORS.secondary};
-        filter: brightness(100%);
-      }
-      ${props.isActive &&
-      css`
-        filter: brightness(100%);
-      `}
-    `}
-`;
+import { IconButtonContainer, Button, ActiveItemIndicator } from "./styled";
 
 const IconButton = ({ children, onClick, isActive }: ButtonProps) => {
   const [theme] = useTheme();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "8px",
-      }}
-    >
+    <IconButtonContainer>
       <Button
         isActive={isActive}
         isDarkMode={theme.isDarkMode}
@@ -79,17 +16,12 @@ const IconButton = ({ children, onClick, isActive }: ButtonProps) => {
       >
         {children}
       </Button>
-      <div
+      <ActiveItemIndicator
         style={{
-          height: "5px",
-          width: "32px",
-          backgroundColor: theme.secondary,
-          border: `1px solid ${theme.secondary}`,
           visibility: isActive ? "visible" : "hidden",
-          filter: theme.isDarkMode ? "brightness(65%)" : "brightness(80%)",
         }}
-      ></div>
-    </div>
+      />
+    </IconButtonContainer>
   );
 };
 export { IconButton };
---
+++ src/components/IconButton/styled.ts
@@ -0,0 +1,78 @@
+import styled, { css } from "styled-components";
+import { ButtonProps } from "types";
+
+import { COLORS } from "utils";
+
+const Button = styled.button<ButtonProps>`
+  padding: 8px;
+  border: 2px solid
+    ${(props) => (props.isDarkMode ? COLORS.darkPrimary : COLORS.primary)};
+  border-radius: 50%;
+  background-color: ${(props) =>
+    props.isActive ? COLORS.secondary : "transparent"};
+  outline-color: transparent;
+  cursor: pointer;
+  color: ${(props) => (props.isActive ? COLORS.secondary : COLORS.primary)};
+  background-image: ${(props) =>
+    props.isActive ? 'url("/images/background.svg")' : undefined};
+
+  ${(props) =>
+    props.isActive &&
+    css`
+      background-image: url("/images/background.svg");
+    `}
+
+  &:focus {
+    outline-offset: 4px;
+    outline-color: ${COLORS.primary};
+  }
+
+  &:hover {
+    background-color: ${COLORS.secondary};
+    background-image: url("/images/background.svg");
+    border-radius: 50%;
+    border-color: ${COLORS.primary};
+    filter: brightness(80%);
+  }
+  ${(props) =>
+    props.isDarkMode &&
+    css`
+      background-image: url("/images/background.svg");
+      background-color: ${COLORS.darkPrimary};
+      border-color: ${COLORS.secondary};
+      background-image: url("/images/background.svg");
+      filter: brightness(65%);
+
+      &:hover {
+        background-color: ${COLORS.primary};
+        background-image: url("/images/background.svg");
+        border-radius: 50%;
+        border-color: ${COLORS.secondary};
+        filter: brightness(100%);
+      }
+      ${props.isActive &&
+      css`
+        filter: brightness(100%);
+      `}
+    `}
+`;
+
+const IconButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
+const ActiveItemIndicator = styled.div`
+  height: 5px;
+  width: 32px;
+  background-color: ${(props) => COLORS.secondary};
+  border: 1px solid ${(props) => COLORS.secondary};
+
+  filter: ${(props) =>
+    props.theme.isDarkMode ? "brightness(65%)" : "brightness(80%)"};
+`;
+
+export { IconButtonContainer, Button, ActiveItemIndicator };
